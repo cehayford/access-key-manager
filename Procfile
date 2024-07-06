d4164b9de66c2a6bc7e2a5fa8c892979abafa788
cehayford <sean.alpha18@gmail.com>
2024-07-06 14:56:37 +0000

    commit project code for generating keys

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'access_key_manager.wsgi'  --log-file -
---
+++ account/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+app_name ='account'
+
+urlpatterns = [
+    path('signup/', signup, name='signup'),
+    path('activate/<str:uidb64>/<str:token>', activate, name='activate'),
+    path('login/', signin, name='login'),
+    path('logout/', signout, name='logout'),
+    path('password_reset/', password_reset, name='reset_page'),
+    path('password_reset/done/', resetPageDone, name='reset_page_done'),
+    path('reset/<uidb64>/<token>', reset_password_confirm, name='password_reset_confirm'),
+]
